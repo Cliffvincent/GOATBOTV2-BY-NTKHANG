5207e0c9b689d23a4016d1d899040af97d9fa077
Mahi-bot1 <161010609+Mahi-bot1@users.noreply.github.com>
2024-02-24 00:05:47 +0600

    Update help2.js

--- scripts/cmds/help2.js
+++ scripts/cmds/help2.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ ♥️| 𝙔𝘼𝙕𝙆𝙔 𝘼𝙄 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[ ♥️| 𝐌𝐀𝐇𝐈 𝐊𝐇𝐀𝐍 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 
 module.exports = {
 	config: {
