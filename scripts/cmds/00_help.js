d2773e0ec34afa4dc1a65a717e892d61b5f5d009
Mahi-bot1 <161010609+Mahi-bot1@users.noreply.github.com>
2024-02-24 00:08:55 +0600

    Update 00_help.js

--- scripts/cmds/00_help.js
+++ scripts/cmds/00_help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🇵🇭|𝗬𝗔𝗭𝗞𝗬 𝗔𝗜";
+const doNotDelete = "🇧🇩|MAHI";
 /** 
 * @author NTKhang
 * @author: do not delete it
