a05c13b7a472a8ff405c64f272efa6b53c8ec36c
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-06-13 08:58:33 +0000

    Hdjdjdj

--- Goat.js
+++ Goat.js
@@ -47,8 +47,8 @@ function validJSON(pathDir) {
 }
 
 const { NODE_ENV } = process.env;
-const dirConfig = path.normalize(`${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`);
-const dirConfigCommands = path.normalize(`${__dirname}/configCommands${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`);
+const dirConfig = path.normalize(`${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.json' : '.json'}`);
+const dirConfigCommands = path.normalize(`${__dirname}/configCommands${['production', 'development'].includes(NODE_ENV) ? '.json' : '.json'}`);
 const dirAccount = path.normalize(`${__dirname}/account${['production', 'development'].includes(NODE_ENV) ? '.txt' : '.txt'}`);
 
 for (const pathDir of [dirConfig, dirConfigCommands]) {
